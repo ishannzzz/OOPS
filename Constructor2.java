b62553177ddebb0eb977c563030bfcedb9c4da9a
ishannzzz <24edlai@rbunagpur.in>
2025-04-06 13:32:25 +0530

    Add files via upload

---
+++ Constructor2.java
@@ -0,0 +1,17 @@
+public class Constructor2 {
+        int modelYear;
+        String modelName;
+
+        public Constructor2(int year, String name) {
+            modelYear = year;
+            modelName = name;
+        }
+
+        public static void main(String[] args) {
+            Constructor2 myCar = new Constructor2(1969, "Mustang");
+            System.out.println(myCar.modelYear + " " + myCar.modelName);
+        }
+    }
+
+
+
